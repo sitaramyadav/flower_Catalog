e179e81eb423a7dea60618a52fc426102fa9de27
sitaram <sitaram@thoughtworks.com>
2016-01-21 13:30:23 +0530

    Seetaram Yadav- data base added.
    What changes have you made?
    
    * data base added for storing the name and comments.
    
    Why do we need this change?
    
    * so that data can be store in database.
    
    What can this change impact?
    
    * It will be sucure.

--- routes.js
+++ routes.js
@@ -1,21 +1,67 @@
 var fs = require('fs');
 var querystring = require('querystring');
-var comments = require('./comments.js');
+var pg=require('pg');
+var connectionString="postgres://postgres:anu$147#@localhost:5000/newflower";
+var toRow = function(entry){
+	return ['<tr>','<td>',entry.name, '</td>','<td>', entry.comments, '</td>','</tr>'].join(' ');
+};
+var generateTable = function(comments){
+	console.log('comments',comments,'comments');
+	return '<table><tr><th>Time</th><th>Name</th><th>Comment</th></tr>' + comments.map(toRow).join(' ') + '</table>';
+};
 
-var template = fs.readFileSync('./templates/guestBook.html', 'utf8');
+var template = fs.readFileSync('./Public/guestBook.html', 'utf8');
 var method_not_allowed = function(req, res){
 	res.statusCode = 405;
 	console.log(res.statusCode);
 	res.end('Method is not allowed');
 };
 var renderGuestBook = function(req, res){
-	res.end(template.replace(/__COMMENTS_TABLE__/, comments.generateTable()));
+	var resultToOut=[];
+	var connectionString="postgres://postgres:seetaram@localhost:5000/newflower";
+		pg.connect(connectionString,function(err,client,done){
+			if(err) return console.log(err,'err');
+				var query=client.query("select *from flowerCatalog");
+				query.on('row',function(row){
+					console.log('row',row,'row');
+					resultToOut.push(row);
+				})
+				query.on('end',function(){
+					client.end();
+					console.log('(resultToOut.rows)',(resultToOut),'(resultToOut.rows)');
+					res.end(template.replace(/__COMMENTS_TABLE__/,generateTable(resultToOut)));
+				})
+			})
 };
 var getComments = function(req,res){
-	res.end(JSON.stringify(comments.getAll()));
+	var resultToOut=[];
+	var connectionString="postgres://postgres:seetaram@localhost:5000/newflower";
+		pg.connect(connectionString,function(err,client,done){
+			if(err) return console.log(err,'err');
+				var query=client.query("select *from flowerCatalog");
+				query.on('row',function(row){
+					resultToOut.push(row);
+				})
+				query.on('end',function(){
+					client.end();
+				res.end(JSON.stringify(resultToOut.rows));
+				})
+			})
 };
+
+var insertInfoIntoDatabase = function(nam,coment){
+	var arr = ['\''+nam+'\'','\''+coment+'\''];
+	var connectionString="postgres://postgres:seetaram@localhost:5000/newflower";
+	pg.connect(connectionString,function(err,client,done){
+		if(err) return console.log('error',err);
+		client.query("insert into flowerCatalog (name,comments) values ("+arr.join(',')+")",function(err,result){
+			if(err) console.log(err,'err');
+			done();
+		})
+	})
+};
+
 var postGuestComment = function(req, res){
-	// debugger;
 	var data = '';
 	req.on('data', function(chunk){
 		data += chunk;
@@ -23,8 +69,8 @@ var postGuestComment = function(req, res){
 	req.on('end', function(){
 		console.log(data);
 		var entry = querystring.parse(data);
-		comments.add(entry);
-		getComments(req,res);
+		insertInfoIntoDatabase(entry.name,entry.comment);
+	
 	});
 };
 
